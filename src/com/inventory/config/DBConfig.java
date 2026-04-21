39b4622e1fb3814e5c436d5494e25012b0fbe2b2
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2026-04-21 13:04:15 +0530

    Update database password in DBConfig.java

--- src/com/inventory/config/DBConfig.java
+++ src/com/inventory/config/DBConfig.java
@@ -3,6 +3,6 @@ package com.inventory.config;
 public class DBConfig {
     public static final String URL      = "jdbc:mysql://localhost:3306/inventory_db?useSSL=false&serverTimezone=UTC";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "root";   // ← Change this
+    public static final String PASSWORD = "your password";   
     public static final String DRIVER   = "com.mysql.cj.jdbc.Driver";
 }
