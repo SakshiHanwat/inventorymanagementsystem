183e7c1e56d4007ccfb88f8c105ed3b8e938322b
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2026-04-21 12:45:07 +0530

    Remove hardcoded password

--- src/com/inventory/config/DBConfig.java
+++ src/com/inventory/config/DBConfig.java
@@ -1,4 +1,8 @@
 package com.inventory.config;
 
-public class DBConnection {
+public class DBConfig {
+    public static final String URL      = "jdbc:mysql://localhost:3306/inventory_db?useSSL=false&serverTimezone=UTC";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root";   // ← Change this
+    public static final String DRIVER   = "com.mysql.cj.jdbc.Driver";
 }
